4f39a9491338ceda4d56510392170e25673ef8b3
Brandon Yeh <lpopo0856@gmail.com>
2025-10-02 16:51:06 +0800

    v3

--- users/feralfile/scripts/ota-update-success-check.sh
+++ users/feralfile/scripts/ota-update-success-check.sh
@@ -21,6 +21,9 @@ CONFIRM_COUNT=0
 VMAGENT_IMPORT_API="http://0.0.0.0:9431/api/v1/import/prometheus"
 FILE_TO_DELETE="/etc/FF_OS_OTA_AUTO_TEST"
 
+CONFIG_FILE="/home/feralfile/ff1-config.json"
+VERSION=$(jq -r '.version' "$CONFIG_FILE" 2>/dev/null)
+
 # Trap: always delete the flag file on exit
 cleanup() {
   if [ -f "$FILE_TO_DELETE" ]; then
@@ -52,7 +55,7 @@ while [ $attempt -lt $MAX_ATTEMPTS ]; do
     
     if [ $CONFIRM_COUNT -ge $CONFIRMATIONS_NEEDED ]; then
       echo "All services confirmed active $CONFIRMATIONS_NEEDED times"
-      METRIC="ff_ota_success_total{} 1"
+      METRIC="ff_ota_update{event=\"success\",target_version=\"$VERSION\"} 1"
       if curl -sS -X POST "$VMAGENT_IMPORT_API" --data-binary "$METRIC" -w "%{http_code}" | grep -q "204"; then
         echo "Successfully sent OTA update done notification to $VMAGENT_IMPORT_API"
       else
