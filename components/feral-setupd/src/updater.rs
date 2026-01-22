932b44cb6d54b43fc1e4008e3fe7c96dc58200f9
Brandon Yeh <lpopo0856@gmail.com>
2026-01-22 11:28:21 +0800

    Revert "don't cache remote version"
    This reverts commit 2c57baa398e4d669ce5b6c599037e3189f3e8259.

--- components/feral-setupd/src/updater.rs
+++ components/feral-setupd/src/updater.rs
@@ -16,6 +16,9 @@ use tokio::{
     time,
 };
 
+// ---------- Cache ----------
+static REMOTE_VERSIONS: OnceLock<UpstreamVersion> = OnceLock::new();
+
 // ---------- Public API ----------
 
 /// Return `Ok(true)` when the running build is **below** the distributor's
@@ -228,6 +231,10 @@ struct UpstreamVersion {
 }
 
 async fn fetch_remote_version() -> Result<UpstreamVersion> {
+    if let Some(versions) = REMOTE_VERSIONS.get() {
+        return Ok(versions.clone());
+    }
+
     let url = format!(
         "{}{}{}",
         cfg::endpoint().await?,
@@ -263,5 +270,6 @@ async fn fetch_remote_version() -> Result<UpstreamVersion> {
         flashing_guide: info.flashing_guide,
         latest_version: Version::parse(&info.latest_version).context("parsing upstream semver")?,
     };
+    REMOTE_VERSIONS.set(versions.clone()).unwrap();
     Ok(versions)
 }
