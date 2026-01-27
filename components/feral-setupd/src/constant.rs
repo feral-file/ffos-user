a6e4683e974249d17412e1a7ac31b4bf41bc6db5
Brandon Yeh <lpopo0856@gmail.com>
2026-01-27 19:29:12 +0800

    update error message

--- components/feral-setupd/src/constant.rs
+++ components/feral-setupd/src/constant.rs
@@ -24,7 +24,7 @@ pub const UPDATER_PROCESS_LOG_FILE: &str = "/var/log/updaterd.log";
 pub const UPDATER_VERSION_CHECK_RETRIES: u32 = 3;
 pub const UPDATER_VERSION_CHECK_RETRY_DELAY: u64 = 2 * 1000; // 2 seconds between retries
 pub const UPDATER_REMOTE_VERSION_REFRESH_INTERVAL: u64 = 60 * 60 * 1000; // 1 hour
-pub const UPDATER_FAILED_TO_CHECK_VERSION_MSG: &str = "Unable to reach update server after multiple attempts. Please verify your internet connection is stable and try again. If this issue persists, contact support@feralfile.com";
+pub const UPDATER_FAILED_TO_CHECK_VERSION_MSG: &str = "FF1 needs to check for critical updates before playing art. If your network is stable and this persists, contact support@feralfile.com - your device may need hands-on help.";
 
 // Bluetooth configuration
 pub const SERVICE_UUID: Uuid = Uuid::from_u128(0xf7826da64fa24e988024bc5b71e0893e_u128);
